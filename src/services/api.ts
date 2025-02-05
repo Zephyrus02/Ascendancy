10aa61609c55314ba12ef9df00a381ae49048082
Zephyrus02 <aneeshraskar@gmail.com>
2025-02-05 22:31:34 +0530

    Shifted to production api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
-const API_URL = import.meta.env.VITE_DEV_URL;
 // const API_URL = import.meta.env.VITE_DEV_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 
 export const createTeam = async (teamData: any) => {
