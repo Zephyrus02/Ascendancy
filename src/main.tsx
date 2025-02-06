6ebe9a28ff2220ceb85e7e881a41c1ca9bd9d64b
Zephyrus02 <aneeshraskar@gmail.com>
2025-02-06 14:58:55 +0530

    set clerk to production

--- src/main.tsx
+++ src/main.tsx
@@ -5,19 +5,21 @@ import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { init } from '@emailjs/browser';
 
-// Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
 
-init('YOUR_PUBLIC_KEY');
+init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <ClerkProvider 
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignOutUrl="/"
+    >
       <App />
     </ClerkProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
